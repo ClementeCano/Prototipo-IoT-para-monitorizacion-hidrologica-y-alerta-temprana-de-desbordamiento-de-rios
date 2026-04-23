5477d8ab05e7b186d270f46b5dcd37617bcb78b8
ClementeCano <clementecano2003@uma.es>
2026-04-23 10:28:07 +0200

    Actualizacion rutas y .env

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Imagen base
+FROM python:3.11-slim
+
+# Carpeta de trabajo
+WORKDIR /app
+
+# Copiar archivos
+COPY . .
+
+# Instalar dependencias
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Comando por defecto
+CMD ["python", "Miranda del Ebro/scheduler.py"]
--- Miranda del Ebro/db_ingest.py
+++ Miranda del Ebro/db_ingest.py
@@ -1,14 +1,18 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
 # =========================
 # CONFIG
 # =========================
+load_dotenv()
+
 conn = psycopg2.connect(
-    dbname="tfg_db",
-    user="postgres",
-    password="admin",
-    host="localhost",
-    port="5432"
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
 )
 
 
